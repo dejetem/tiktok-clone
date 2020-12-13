50aac87a3e7be0d97239985bfbb4ebc5cc1cb688
dejetem <enyeoluwarotimisimon@gmail.com>
2020-12-13 19:15:17 +0100

    tik tok clone by simon

---
+++ data.js
@@ -0,0 +1,23 @@
+const data = [
+   { 
+    url:"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    channel:'iam_simondeje',
+    desc:'happy coding' ,
+    song:'tik tok clone',
+    likes:"676",
+    messages:"451",
+    shares:"402",
+   },
+   { 
+    url:"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+    channel:'iam_simondeje',
+    desc:'happy coding' ,
+    song:'tik tok clone',
+    likes:"67",
+    messages:"45",
+    shares:"40",
+   }
+
+]
+
+export default data;
---
+++ server.js
@@ -0,0 +1,53 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from 'cors';
+//import data from './data.js';
+import videos from './dbModel.js';
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app = express();
+const port = process.env.PORT
+const db = process.env.DB_HOST
+app.use(express.json());
+const corsOptions = {
+    origin: 'https://tiktok-clone-59d8d.web.app',
+    optionsSuccessStatus: 200 
+  }
+
+mongoose.connect(db,{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+
+
+app.get('/',cors(corsOptions),(req,res) => 
+  res.status(200).send('Hello tik tok'));
+
+/*app.get('/v1/posts',cors(corsOptions),(req,res) => 
+  res.status(200).send(data));*/
+
+app.post('/v2/posts',cors(corsOptions),(req,res) => {
+    const dbVideos =req.body
+    videos.create(dbVideos, (err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else{
+            res.status(201).send(data);
+        }
+    })
+});
+
+app.get('/v2/posts',cors(corsOptions),(req,res) => {
+    videos.find((err, data) => {
+        if(err) {
+            res.status(500).send(err);
+        }else {
+            res.status(200).send(data);
+        }
+    })
+})
+
+app.listen(port, () => console.log(`App listenning to port ${port}`));
+//xrzhnVDcPJnoOFhd
