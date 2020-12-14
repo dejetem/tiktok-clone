7f390ff66dde16c371e5a882b6b097d5121ae9ec
dejetem <enyeoluwarotimisimon@gmail.com>
2020-12-14 03:01:13 +0100

    Added a Procfile.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT
 const db = process.env.DB_HOST
 app.use(express.json());
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: 'https://tiktok-clone-59d8d.web.app',
     optionsSuccessStatus: 200 
   }
 
