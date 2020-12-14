cb8b36ef46a550208ce282e00a518ff34484a9a8
dejetem <enyeoluwarotimisimon@gmail.com>
2020-12-14 02:54:00 +0100

    Added a Procfile.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from 'cors';
-import cool from 'cool-ascii-faces';
 //import data from './data.js';
 import videos from './dbModel.js';
 import dotenv from 'dotenv'
@@ -11,16 +10,10 @@ const app = express();
 const port = process.env.PORT
 const db = process.env.DB_HOST
 app.use(express.json());
-app.use(cors());
-app.use((req,res,next) => {
-    res.setHeader("Allow-Control-Allow-Origin", "*");
-    res.setHeader("Allow-Control-Allow-Headers", "*");
-    next();
-}) 
-/*const corsOptions = {
+const corsOptions = {
     origin: 'http://localhost:3000',
     optionsSuccessStatus: 200 
-  }*/
+  }
 
 mongoose.connect(db,{
     useNewUrlParser: true,
@@ -29,16 +22,13 @@ mongoose.connect(db,{
 })
 
 
-app.get('/',(req,res) => 
-  res.status(200).send('Hello tik tok'));
-
-app.get('/cool',(req,res) => 
+app.get('/',cors(corsOptions),(req,res) => 
   res.status(200).send('Hello tik tok'));
 
 /*app.get('/v1/posts',cors(corsOptions),(req,res) => 
   res.status(200).send(data));*/
 
-app.post('/v2/posts',(req,res) => {
+app.post('/v2/posts',cors(corsOptions),(req,res) => {
     const dbVideos =req.body
     videos.create(dbVideos, (err, data) => {
         if (err) {
@@ -49,7 +39,7 @@ app.post('/v2/posts',(req,res) => {
     })
 });
 
-app.get('/v2/posts',(req,res) => {
+app.get('/v2/posts',cors(corsOptions),(req,res) => {
     videos.find((err, data) => {
         if(err) {
             res.status(500).send(err);
